01a95eb71ccf4541e8be47e89a189eaf3ee513ff
Preetham206 <20ME0040@vvce.ac.in>
2021-03-06 05:22:55 +0000

    S

--- Program-7.c
+++ Program-7.c
@@ -8,9 +8,10 @@
 	  int a[10],i,n;
 	  printf("Enter size of the array and the array elements");
 	  scanf("%d",&n);
-	  printf("Array elements in reverse order:");
-   for(i=0;i<n;i++);
+	  printf("enter array elements");
+   for(i=0;i<n;i++)
 	  scanf("%d",&a[i]);//missing code
+	  printf("Array elements in reverse order :");
    for(i=n-1;i>=0;--i) //missing code
 	  {
 	  printf("%d ",a[i]);
