8f4930d10b68ad8c13ed18b15bae53ad1dc9750e
Preetham206 <>
2021-04-12 14:12:57 +0000

    S

--- Program-5.c
+++ Program-5.c
@@ -2,24 +2,27 @@
 //OUTPUT - Sum of elements=10
 
 
-#include <stdio.h>
+#include<stdio.h>
 
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
    int a[10],i,sum=0,n;
    printf("Enter number of elements in array and the array elements");
    scanf("%d", &n);
-   
-  // printf("Enter %d elements\n",n);
-   
+   printf("enter the elements of the array");
    for (i = 0; i < n; i++)
    {
       scanf("%d", &a[i]);
       sum=sum+a[i];
    }
- printf("Sum of elements=%d", sum);
+  // printf("Enter %d elements\n", n);
+   for (i = 0; i < n; i++)
+   {
+     sum=sum+a[i];
+   }
+ printf("Sum of elements=%d",sum );
    
   return 0;
 }
